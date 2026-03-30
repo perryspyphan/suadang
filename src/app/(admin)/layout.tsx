8cee3105da41c2f1e52e10539bb0fdaff93271de
Perry <haileyxjustinbeiber@gmail.com>
2026-03-30 08:21:50 +0700

    Nội dung ghi chú về thay đổi của bạn

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,9 +1,6 @@
-import Header from '@/presentation/components/layout/Header'
-
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
       {children}
     </>
   )
